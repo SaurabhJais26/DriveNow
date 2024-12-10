a1fc3ae6082e3517a5a31e3c2c6928e79db62ba3
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-10 18:57:46 +0530

    Added MapViewState as enum and configured the code accordingly to manage our map view.

--- DriveNow/Core/LocationSearch/Views/LocationSearchView.swift
+++ DriveNow/Core/LocationSearch/Views/LocationSearchView.swift
@@ -11,7 +11,7 @@ struct LocationSearchView: View {
     
     @State private var startLocationText = ""
     
-    @Binding var showLocationSearchView: Bool
+    @Binding var mapState: MapViewState
     @EnvironmentObject var viewModel: LocationSearchViewModel
     
     var body: some View {
@@ -59,7 +59,7 @@ struct LocationSearchView: View {
                         LocationSearchResultCell(title: result.title, subtitle: result.subtitle)
                             .onTapGesture {
                                 viewModel.selectLocation(result)
-                                showLocationSearchView.toggle()
+                                mapState = .locationSelected
                             }
                     }
                 }
@@ -70,5 +70,5 @@ struct LocationSearchView: View {
 }
 
 #Preview {
-    LocationSearchView(showLocationSearchView: .constant(false))
+    LocationSearchView(mapState: .constant(.searchingForLocation))
 }
---
+++ DriveNow/Models/MapViewState.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewState.swift
+//  DriveNow
+//
+//  Created by Saurabh Jaiswal on 10/12/24.
+//
+
+import Foundation
+
+enum MapViewState {
+    case noInput
+    case searchingForLocation
+    case locationSelected
+}
