cdd2a2edc6386cb6c2b7f4049bf1942392ac86cb
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-11 12:34:54 +0530

    Added the logic to present the ride request view after the location is selected.

--- DriveNow/Core/Trips/RideRequestView.swift
+++ DriveNow/Core/Trips/RideRequestView.swift
@@ -13,6 +13,7 @@ struct RideRequestView: View {
             Capsule()
                 .foregroundColor(Color(.systemGray5))
                 .frame(width: 48, height: 6)
+                .padding(.top, 8)
             
             // trip info view
             HStack {
@@ -49,6 +50,7 @@ struct RideRequestView: View {
                     HStack {
                         Text("Coffee Lovers")
                             .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.black)
                         
                         Spacer()
                         
@@ -141,7 +143,9 @@ struct RideRequestView: View {
             }
 
         }
+        .padding(.bottom, 24)
         .background(.white)
+        .cornerRadius(16)
     }
 }
 
