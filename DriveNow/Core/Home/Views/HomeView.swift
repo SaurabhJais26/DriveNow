835015bb0d456d52e14977d14c15476ff0748dbe
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-15 14:19:06 +0530

    Handled the tap for opening the side menu with setting the offset for the mapview.

--- DriveNow/Core/Home/Views/HomeView.swift
+++ DriveNow/Core/Home/Views/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct HomeView: View {
     
     @State private var mapState = MapViewState.noInput
+    @State private var showSideMenu = false
     @EnvironmentObject var locationViewModel: LocationSearchViewModel
     @EnvironmentObject var authViewModel: AuthViewModel
     
@@ -18,40 +19,53 @@ struct HomeView: View {
             if authViewModel.userSession == nil {
                 LoginView()
             } else {
-                ZStack(alignment: .bottom) {
-                    ZStack(alignment: .top) {
-                        DNMapViewRepresentable(mapState: $mapState)
-                            .ignoresSafeArea()
-                        
-                        if mapState == .searchingForLocation {
-                            LocationSearchView(mapState: $mapState)
-                        } else if mapState == .noInput{
-                            LocationSearchActivationView()
-                                .padding(.top, 72)
-                                .onTapGesture {
-                                    withAnimation(.spring()) {
-                                        mapState = .searchingForLocation
-                                    }
-                                }
+                ZStack {
+                    if showSideMenu {
+                        SideMenuView()
+                    }
+                    mapView
+                        .offset(x: showSideMenu ? 316 : 0)
+                        .shadow(color: showSideMenu ? .black : .clear, radius: 10)
+                }
+            }
+        }
+    }
+}
+
+extension HomeView {
+    var mapView: some View {
+        ZStack(alignment: .bottom) {
+            ZStack(alignment: .top) {
+                DNMapViewRepresentable(mapState: $mapState)
+                    .ignoresSafeArea()
+                
+                if mapState == .searchingForLocation {
+                    LocationSearchView(mapState: $mapState)
+                } else if mapState == .noInput{
+                    LocationSearchActivationView()
+                        .padding(.top, 72)
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                mapState = .searchingForLocation
+                            }
                         }
-                        
-                        MapViewActionButton(mapState: $mapState)
-                            .padding(.leading)
-                            .padding(.top, 4)
-                    }
-                    
-                    if mapState == .locationSelected  || mapState == .polylineAdded {
-                        RideRequestView()
-                            .transition(.move(edge: .bottom))
-                    }
                 }
-                .edgesIgnoringSafeArea(.bottom)
-                .onReceive(LocationManager.shared.$userLocation) { location in
-                    if let location = location {
-                        print("DEBUG: User location in home view is \(location)")
-                        locationViewModel.userLocation = location
-                    }
-                }
+                
+                MapViewActionButton(mapState: $mapState, showSideMenu: $showSideMenu)
+                    .padding(.leading)
+                    .padding(.top, 4)
+            }
+            
+            if mapState == .locationSelected  || mapState == .polylineAdded {
+                RideRequestView()
+                    .transition(.move(edge: .bottom))
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .onReceive(LocationManager.shared.$userLocation) { location in
+            if let location = location {
+                print("DEBUG: User location in home view is \(location)")
+                locationViewModel.userLocation = location
             }
         }
     }
@@ -59,4 +73,5 @@ struct HomeView: View {
 
 #Preview {
     HomeView()
+        .environmentObject(AuthViewModel())
 }
--- DriveNow/Core/Home/Views/MapViewActionButton.swift
+++ DriveNow/Core/Home/Views/MapViewActionButton.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MapViewActionButton: View {
     @Binding var mapState: MapViewState
+    @Binding var showSideMenu: Bool
     @EnvironmentObject var viewModel: LocationSearchViewModel
     @EnvironmentObject var authViewModel: AuthViewModel
     
@@ -27,14 +28,13 @@ struct MapViewActionButton: View {
                 .shadow(color: .black, radius: 6)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
-
+        
     }
     
     func actionForState(_ state: MapViewState){
         switch state {
         case .noInput:
-            print("DEBUG: No Input")
-//            authViewModel.signOut()
+            showSideMenu.toggle()
         case .searchingForLocation:
             mapState = .noInput
         case .locationSelected, .polylineAdded:
@@ -57,5 +57,5 @@ struct MapViewActionButton: View {
 }
 
 #Preview {
-    MapViewActionButton(mapState: .constant(.noInput))
+    MapViewActionButton(mapState: .constant(.noInput), showSideMenu: .constant(false))
 }
