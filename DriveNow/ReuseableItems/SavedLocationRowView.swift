03e5850d0aa89303a3b221575855f99faf3b1343
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-16 15:38:31 +0530

    Fetched the saved location to show up on the screen and configured some ui layouts.

--- DriveNow/ReuseableItems/SavedLocationRowView.swift
+++ DriveNow/ReuseableItems/SavedLocationRowView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SavedLocationRowView: View {
     let viewModel: SavedLocationViewModel
+    let user: User
     
     var body: some View {
         HStack(spacing: 12) {
@@ -22,7 +23,7 @@ struct SavedLocationRowView: View {
                     .font(.system(size: 15, weight: .semibold))
                     .foregroundColor(Color.theme.primaryTextColor)
                 
-                Text(viewModel.subtitle)
+                Text(viewModel.subtitle(forUser: user))
                     .font(.system(size: 14))
                     .foregroundColor(.gray)
                 
@@ -31,6 +32,6 @@ struct SavedLocationRowView: View {
     }
 }
 
-#Preview {
-    SavedLocationRowView(viewModel: .home)
-}
+//#Preview {
+//    SavedLocationRowView(viewModel: .home)
+//}
